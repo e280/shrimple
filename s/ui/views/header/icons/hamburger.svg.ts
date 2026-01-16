d94fa19883215cc8b058c795636bf2146922e8c9
Chase Moskal <chasemoskal@gmail.com>
2026-01-15 18:11:53 -0800

    shrimplify: header

--- s/ui/views/header/icons/hamburger.svg.ts
+++
@@ -1,7 +0,0 @@
-import {html} from "lit"
-
-export const hamburger = html`
-	<svg class="menu-icon" viewBox="0 0 24 24" aria-hidden="true">
-		<path d="M3 6h18M3 12h18M3 18h18" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-	</svg>
-`
